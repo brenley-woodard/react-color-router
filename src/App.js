9eea3fe073dd9d8c6cb192cfc20ac5005321d686
brenley-woodard <brenleyew@gmail.com>
2023-01-24 14:15:53 -0800

    not found "page" appears if url has incorrect rgb input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Main from './components/Main/Main.js';
 import Nav from './components/Nav/Nav';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Route exact path="/">
           <Redirect to="/237/184/139" />
         </Route>
-        <Route path="/:r/:g/:b" component={Main} />
+        <Route exact path="/:r/:g/:b" component={Main} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Display from '../Display/Display';
-import Nav from '../Nav/Nav';
 
 export default function Main() {
   return (
