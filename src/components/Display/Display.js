cac7f95113ed8910ba687064c806bae8929f1d50
brenley-woodard <brenleyew@gmail.com>
2023-01-24 12:01:10 -0800

    rbg display is changing in correspondence to url

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -5,7 +5,7 @@ export default function Display() {
   const { r, g, b } = useParams();
   return (
     <div style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
-      <p style={{ height: 400 }}>Display</p>
+      <p style={{ height: 1000 }}> rgb({r}, {g}, {b})</p>
     </div>
   );
 }
